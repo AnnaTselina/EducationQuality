034aa2ba41633c1eb3b7595fa27e25a96d220e84
Anna <annacelina@users.noreply.github.com>
2020-10-26 20:48:02 +0300

    f

--- model.js
+++ model.js
@@ -66,8 +66,7 @@ export default class Model {
                             }
                             window.alert("Ошибка: " + errorMessage);
                     })
-
-            )       
+            );       
         })   
     }
 
