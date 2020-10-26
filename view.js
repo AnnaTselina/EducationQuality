64f80f860f4d85a5fd6b9324fe4bcfaf7bc600f6
Anna <annacelina@users.noreply.github.com>
2020-10-26 16:47:28 +0300

    Изменена подгрузка фотографий

--- view.js
+++ view.js
@@ -360,7 +360,7 @@ export default class View {
         let card = document.createElement('div');        
         card.className +="littleCard";
         let img = document.createElement('img');
-        img.src = data["photoURL"];
+        img.src = "pic/teachers/" + data["photoURL"] + ".jpg";
         img.className += 'personalPhoto';
         card.appendChild(img);
         let otherText = '<p>'+  data['Дисциплина'] +'</p><p>'+ data['Преподаватель']+'</p><p>'+ data['Тип занятия']+ '</p><p><span>Общая оценка: </span>'+ data['Общая оценка']+'</p><p><span>Количество оценок: </span>'+ data['Количество оценивших']+'</p>'
@@ -384,8 +384,8 @@ export default class View {
         modal.className += 'modal-bigCard';
         let modalContent = document.createElement('div');
         modalContent.className += 'bigCard';
-        let img = document.createElement('img');    
-        img.src = d["photoURL"];
+        let img = document.createElement('img');  
+        img.src = "pic/teachers/" + d["photoURL"] + ".jpg"
         img.className += 'personalPhoto';
         modalContent.appendChild(img);
         let otherText = '<table><tr><td><span>Дисциплина: </span></td><td>'+ d['Дисциплина'] +'</td></tr><tr><td><span>Преподаватель: </span></td><td>'+ d['Преподаватель'] +'</td></tr><tr><td><span>Тип занятия: </span></td><td>'+ d['Тип занятия'] +'</td></tr><tr><td><span>Общая оценка: </span></td><td>' + d['Общая оценка']+ '</td></tr><tr><td><span>Количество оценивших: </span></td><td>'+d['Количество оценивших']+'</td></tr></table>';
